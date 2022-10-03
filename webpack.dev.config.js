f836ee1c186799e81b8a3d71a794e1cb65dd606f
rivalcoba <ivan.rivalcoba@gmail.com>
2022-10-03 07:52:32 -0500

    Separando configuradores webpack de front-end

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -7,8 +7,6 @@ const path = require('path');
 // Exportamos un objeto de configuración
 // que sera usado por webpack
 module.exports = {
-  // 0. Estableciendo el modo produccion
-  mode: 'production',
   // 1. El archivo de entrada o indexador
   entry: "./client/index.js",
   // 2. Especificar el archivo de salida
@@ -18,4 +16,13 @@ module.exports = {
     // 2.2 Nombre del archivo de salida
     filename: "bundle.js"
   },
+  // 3. Configurando el servidor de desarrollo
+  devServer: {
+    // 3.1 Folder de archivos estáticos
+    static: path.join(__dirname, "public"),
+    // 3.2 Puerto del servidor de desarrollo
+    port: 8080,
+    // 3.3 Definiendo el host
+    host: "localhost"
+  }
 }
