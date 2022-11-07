5a56e79eb56b8bf99f34945ea09baf083756af77
rivalcoba <ivan.rivalcoba@gmail.com>
2022-11-07 08:16:30 -0600

    Instalando Materielize CSS

--- client/index.js
+++ client/index.js
@@ -1,5 +1,12 @@
 // Cargado estilos
 import './stylesheet/myStyles.css';
 import './stylesheet/style.css';
+// Importandoe Estilos de Materialize CSS
+import 'materialize-css/dist/css/materialize.css';
+// Importando scripts de Materialize
+import 'materialize-css/dist/js/materialize';
+
+// Inicializando Scripts de Materialize para interactividad
+M.AutoInit();
 
 console.log('Webpack Working Middleware!!! 📦');
